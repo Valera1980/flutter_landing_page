c262a9eb79f0ca509e0fa42443610fce06e40f83
Valera <gavrilow777@gmail.com>
2020-04-21 18:54:52 +0300

    ad columns

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amber,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+            Container(
+              width: 100.0,
+              color: Colors.white,
+              child: Text('Container 1' ),
+            ),
+            SizedBox(
+              child: Text('SizedBox'),
+              width: 30.0,
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+          ],),
+        )
       ),
     );
   }

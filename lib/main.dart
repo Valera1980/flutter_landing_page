2805fc5024025c9453856ed83da6ae307debbd2e
Valera <gavrilow777@gmail.com>
2020-04-21 20:15:26 +0300

    add separate line

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,13 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'SourceSansPro',
                     letterSpacing: 2.5),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
                   color: Colors.white,
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
